b35d4c048da8292209622b49aa2555266f49ef2d
Gregor <panek@Mini-von-Gregor.fritz.box>
2026-08-27 20:54:04 +0200

    Improve code coverage, update readme.md and add changelog.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import globals from 'globals';
 
 export default [
   {
-    ignores: ['node_modules/**', 'test/assets/**']
+    ignores: ['node_modules/**', 'test/assets/**', 'coverage/**']
   },
   {
     files: ['**/*.js'],
