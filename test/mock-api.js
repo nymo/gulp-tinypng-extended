243c0c926f8485fe37274797a88e02eaec9c150c
Gregor <panek@Mini-von-Gregor.fritz.box>
2026-08-27 11:22:47 +0200

    Refresh project metadata and tooling
    Update GitHub Actions, regenerate the npm lockfile, refresh license
    attributions, and add repository links and API test setup.

---
+++ test/mock-api.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+    nock = require('nock'),
+    compressedImage = fs.readFileSync(__dirname + '/assets/image_small.png');
+
+module.exports = function mockTinyPngApi() {
+    nock('https://api.tinify.com')
+        .persist()
+        .post('/shrink')
+        .reply(201, {
+            output: {
+                url: 'https://api.tinify.com/output'
+            }
+        });
+
+    nock('https://api.tinify.com')
+        .persist()
+        .get('/output')
+        .reply(200, compressedImage);
+};
+
